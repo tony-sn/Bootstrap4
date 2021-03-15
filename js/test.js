b54e17f7da71ffdb2caf05d934e2f0a79a0dd87c
tony-sn <tony.sn@outlook.com>
2021-03-15 20:37:33 +1100

    edit news section

---
+++ js/test.js
@@ -0,0 +1,10 @@
+var name = 'Nguyen Son Tung';
+var address = '16 Johnson St';
+var myDaughterName = 'Alana';
+const PI = 3.1414;
+var banKinh = 20;
+var chuVi = banKinh * 2 * PI;
+
+
+console.log(name);
+console.log(chuVi);
